734c2228640aea50ecae6b6badafe4ab4d6aa426
Alex T <tonko_a_a@mail.ru>
2018-04-24 15:54:26 +0300

    исправление светлой темы

--- app/src/main/java/tonko/com/client/view/SplashActivity.kt
+++ app/src/main/java/tonko/com/client/view/SplashActivity.kt
@@ -1,12 +1,13 @@
 package tonko.com.client.view
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.support.v7.app.AppCompatActivity
 import tonko.com.client.R
 
-class SplashActivity : AppCompatActivity() {
+class SplashActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
