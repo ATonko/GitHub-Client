4d88d998f72a38d337d854eec22820f275befe16
Alexander Tonko <Tonko.A.A@sberbank.ru>
2020-02-28 21:50:42 +0300

    binds for repo list

--- app/src/main/java/tonko/com/client/dagger/RepoListComponent.kt
+++ app/src/main/java/tonko/com/client/dagger/RepoListComponent.kt
@@ -1,18 +1,17 @@
 package tonko.com.client.dagger
 
+import dagger.Binds
 import dagger.Module
-import dagger.Provides
 import dagger.Subcomponent
-import tonko.com.client.model.interfaces.IRepoRepository
 import tonko.com.client.presenters.RepoListPresenter
 import tonko.com.client.presenters.interfaces.IRepoListPresenter
 import tonko.com.client.view.RepoListActivity
 
 @Module
-class RepoListModule {
+abstract class RepoListModule {
 
-    @Provides
-    fun providePresenter(repository: IRepoRepository): IRepoListPresenter = RepoListPresenter(repository)
+    @Binds
+    abstract fun bindsPresenter(impl: RepoListPresenter): IRepoListPresenter
 }
 
 @Subcomponent(modules = [RepoListModule::class])
--- app/src/main/java/tonko/com/client/presenters/RepoListPresenter.kt
+++ app/src/main/java/tonko/com/client/presenters/RepoListPresenter.kt
@@ -7,8 +7,9 @@ import tonko.com.client.api.json_responses.Repos
 import tonko.com.client.model.interfaces.IRepoRepository
 import tonko.com.client.presenters.interfaces.IRepoListPresenter
 import tonko.com.client.view.interfaces.BaseListView
+import javax.inject.Inject
 
-class RepoListPresenter(private val repository: IRepoRepository) : BasePresenter<BaseListView<Repos>>(), IRepoListPresenter
+class RepoListPresenter @Inject constructor(private val repository: IRepoRepository) : BasePresenter<BaseListView<Repos>>(), IRepoListPresenter
 {
     override fun getList(login: String)
     {
